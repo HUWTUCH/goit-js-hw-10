773c1211869cf3d004bfb3c9933a3a32662b4616
Danik005 <141757644+HUWTUCH@users.noreply.github.com>
2024-02-05 22:28:02 +0200

    tasck-2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+import iziToast from 'izitoast';
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      if (state === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    },delay)
+  })
+}
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = parseInt(e.target.elements.delay.value, 10);
+  const state = e.target.elements.state.value;
+
+  createPromise(delay, state)
+    .then(result => {
+      iziToast.success({
+        title: '',
+        message: `${result}`,
+        position: 'topRight',
+      });
+    })
+    .catch(err => {
+      iziToast.error({
+        title: '',
+        message: `${err}`,
+        position: 'topRight',
+      });
+    });
+});
